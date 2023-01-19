8aff9c12401d37b14f1333ce69f39518bfa60049
Romeoscript <romeobourne211@gmail.com>
2023-01-19 09:21:21 +0100

    feat:installed tailwind css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <>
+      <ul>
+        <li>Mentorship</li>
+        <li>Signals</li>
+        <li>Exchange</li>
+        <li>About us </li>
+      </ul>
+    </>
+  );
+};
+
+export default Nav;
