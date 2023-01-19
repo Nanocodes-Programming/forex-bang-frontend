ebc31b9cd10df8d5842f841628e4002c86ad9966
Romeoscript <romeobourne211@gmail.com>
2023-01-19 09:12:53 +0100

    router done and working fine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Exchanges from "./pages/Exchanges";
+import Home from "./pages/Home";
+import Mentor from "./pages/Mentor";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/mentorship",
+    element: <Mentor />,
+  },
+  {
+    path: "/exchange",
+    element: <Exchanges />,
+  },
+]);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
