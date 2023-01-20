58e32273933b141d5d90b3d83020e5356b2ead8f
Romeoscript <romeobourne211@gmail.com>
2023-01-20 13:44:10 +0100

    style:started the styling of the about page

--- src/pages/Mentor.jsx
+++ src/pages/Mentor.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Layout from "../components/Layout";
+import Wrapper from "../components/Wrapper";
+import chart from '../assets/img/chart3-svgrepo-com 1.png'
 
 const Mentor = () => {
-  return <div>mentor</div>;
+  return (
+    <Layout>
+      <Wrapper>
+        {/* home wrapper */}
+        <div className="w-[40%] h-full flex flex-col justify-end p-[4rem] items-start">
+          <img src={chart} alt="" />
+          <h1 className="text-4xl font-bold text-white">Mentorship Program</h1>
+          <p className="text-2xl text-white   my-4">
+            Exclusive Mentorship Program Get the best One-on-One Private
+            Training you could ever imagine both online and offline with LumenFx
+            Expert Team.
+          </p>
+        </div>
+        {/* home wrapper */}
+        {/* services starts here */}
+      </Wrapper>
+    </Layout>
+  );
 };
 
 export default Mentor;
