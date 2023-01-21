d2d1f3155a45a5f14a98be3bb6a53be67b30d90e
Romeoscript <romeobourne211@gmail.com>
2023-01-21 14:54:20 +0100

    exhange

--- src/pages/Exchanges.jsx
+++ src/pages/Exchanges.jsx
@@ -10,13 +10,24 @@ const Exchanges = () => {
         {/* home wrapper */}
         <div className="w-[40%] h-full flex flex-col justify-end p-[4rem] items-start">
           <img src={dollar} alt="" />
-          <h1 className="text-4xl font-bold text-white">Exchange With Lumen Forex Academy</h1>
+          <h1 className="text-4xl font-bold text-white">
+            Exchange With Lumen Forex Academy
+          </h1>
         </div>
         {/* home wrapper */}
         {/* services starts here */}
       </Wrapper>
       <div className=" bg-[url('/src/assets/img/forexbg.png')] pb-[5rem]  bg-cover bg-center">
-        <Card />
+        <Card
+          price="100"
+          lists={[
+            "Sell Bitcoins, Usdt and E-Wallet Funds (Perfect Money,  Skrill, Neteller,  Payoneer Funds) with LumenFx at good rates",
+
+            "» Good Rate",
+            "» Fast and Secured",
+            "» Minimum Trade Amount ✅",
+          ]}
+        />
       </div>
     </Layout>
   );
