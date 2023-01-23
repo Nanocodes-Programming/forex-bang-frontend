3bf753c0d07d5d7225a9bc1218890351fb69ea43
Romeoscript <romeobourne211@gmail.com>
2023-01-23 17:50:34 +0100

    counter done

--- src/components/ScrollCounter.jsx
+++ src/components/ScrollCounter.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const ScrollCounter = () => {
+  const [viewPortEntered, setViewPortEntered] = useState(false);
   return (
-    <div className="flex md:flex-row flex-col justify-between w-[80%] m-auto md:my-auto my-[1rem] text-center text-white z-10 relative">
-      <aside>
-        <h1 className="text-5xl mb-3 font-bold">180+</h1>
-        <p>Student</p>
-      </aside>
-      <aside >
-        <h1 className="text-5xl mb-3 font-bold">98%</h1>
-        <p>Success Rate</p>
-      </aside>
-      <aside>
-        <h1 className="text-5xl mb-3 font-bold">500+</h1>
-        <p>Subscribers</p>
-      </aside>
-      <aside>
-        <h1 className="text-5xl mb-3 font-bold">50+</h1>
-        <p>Signals daily</p>
-      </aside>
-    </div>
+    <ScrollTrigger
+      onEnter={() => setViewPortEntered(true)}
+      onExit={() => setViewPortEntered(false)}
+    >
+      <div className="flex md:flex-row flex-col justify-between w-[80%] m-auto md:my-auto my-[1rem] text-center text-white z-10 relative">
+        <aside>
+          <h1 className="text-5xl mb-3 font-bold">
+            {viewPortEntered && <CountUp end={180} duration={2} />} +
+          </h1>
+          <p>Student</p>
+        </aside>
+        <aside>
+          <h1 className="text-5xl mb-3 font-bold">
+            {viewPortEntered && <CountUp end={98} duration={2} />}%
+          </h1>
+          <p>Success Rate</p>
+        </aside>
+        <aside>
+          <h1 className="text-5xl mb-3 font-bold">
+            {viewPortEntered && <CountUp end={500} duration={2} />}+
+          </h1>
+          <p>Subscribers</p>
+        </aside>
+        <aside>
+          <h1 className="text-5xl mb-3 font-bold">
+            {viewPortEntered && <CountUp end={50} duration={5} />}+
+          </h1>
+          <p>Signals daily</p>
+        </aside>
+      </div>
+    </ScrollTrigger>
   );
 };
 
