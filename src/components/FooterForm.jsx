698d749a1987c820646617be751559fbf0baa04d
Romeoscript <romeobourne211@gmail.com>
2023-02-02 14:32:36 +0100

    feat:added the new information

--- src/components/FooterForm.jsx
+++ src/components/FooterForm.jsx
@@ -8,7 +8,7 @@ const FooterForm = () => {
        <p>submit your email to get the latest updates on <br /> forex and other related <br /> subjects.</p>
         <input
           type="email"
-          className=" rounded-[10px] my-4 p-2 w-full block bg-[#fff] outline-none"
+          className=" rounded-[10px] my-4 p-2 w-full block bg-[#fff] text-black outline-none"
           placeholder=" Enter your Email"
         />
         <button className="bg-[#2ECBF1] hover:bg-[#2ECBF1] hover:text-[#030C4B] px-[2rem] py-[0.5rem] rounded-lg"> send </button>
