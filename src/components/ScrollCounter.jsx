dda60b86a50c99d225d18ac863db11c01dde85c2
Romeoscript <romeobourne211@gmail.com>
2023-02-10 04:52:18 +0100

    scroll counter done

--- src/components/ScrollCounter.jsx
+++ src/components/ScrollCounter.jsx
@@ -10,27 +10,27 @@ const ScrollCounter = () => {
       onExit={() => setViewPortEntered(false)}
     >
       <div className="flex md:flex-row flex-col justify-between w-[80%] m-auto md:my-auto my-[1rem] text-center text-white z-10 relative">
-        <aside>
-          <h1 className="text-5xl mb-3 font-bold">
-            {viewPortEntered && <CountUp end={180} duration={2} />} +
+        <aside className="border-[2px] p-[1rem]">
+          <h1 className="text-5xl mb-3 font-bold ">
+            {viewPortEntered && <CountUp end={1} duration={2} />}K+
           </h1>
-          <p>Student</p>
+          <p>Testimony</p>
         </aside>
-        <aside>
+        <aside className="border-[2px] p-[1rem] md:m-0 my-[0.5rem]">
           <h1 className="text-5xl mb-3 font-bold">
             {viewPortEntered && <CountUp end={98} duration={2} />}%
           </h1>
           <p>Success Rate</p>
         </aside>
-        <aside>
+        <aside className="border-[2px] p-[1rem] md:m-0 my-[0.5rem]">
           <h1 className="text-5xl mb-3 font-bold">
-            {viewPortEntered && <CountUp end={500} duration={2} />}+
+            {viewPortEntered && <CountUp end={10} duration={2} />}K+
           </h1>
           <p>Subscribers</p>
         </aside>
-        <aside>
+        <aside className="border-[2px] p-[1rem]">
           <h1 className="text-5xl mb-3 font-bold">
-            {viewPortEntered && <CountUp end={50} duration={2} />}+
+         {viewPortEntered && <CountUp end={5} duration={2} />}+
           </h1>
           <p>Signals daily</p>
         </aside>
